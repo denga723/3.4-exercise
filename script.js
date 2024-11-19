42ddfb585558143686dca0b41e59d39d9c7220c7
denga723 <denga723@newschool.edu>
2024-11-19 14:56:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.thumb').forEach(thumb => {
     thumb.addEventListener('click', function () {
       const mainImage = document.getElementById('displayed-image');
-      mainImage.src = this.src; // Update the main image source
+      mainImage.src = this.src; 
     });
   });
   
